2858b09c60864595e16fce6253b6a32f1e016220
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r2s2.42lisboa.com>
2023-09-08 16:35:36 +0100

    stringFuncs beginning

--- srcs/stringFuncs.cpp
+++ srcs/stringFuncs.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
-#include <vector>
-
+#include <stringFuncs.hpp>
 
 std::string ltrim(std::string str)
 {
     std::size_t found = str.find_first_not_of(" \t");
+
+    if (found == std::string::npos)
+        return str;
 
     str = str.substr(found);
 
@@ -13,11 +14,9 @@ std::string ltrim(std::string str)
 
 std::string trim(std::string str)
 {
-    std::size_t found = str.find_first_not_of(" \t");
+    str = ltrim(str);
 
-    str = str.substr(found);
-
-    found = str.find_last_not_of(" \t");
+    std::size_t found = str.find_last_not_of(" \t");
     str = str.substr(0, found + 1);
 
     return str;
@@ -40,32 +39,18 @@ std::vector<std::string>    split(std::string str)
 {
     std::vector<std::string>    divided;
 
-        str = trim(str);
+    str = trim(str);
 
     while (std::size_t found = str.find(" "))
     {
+        if (found == std::string::npos)
+            break;
+
         divided.push_back(str.substr(0, found));
 
         str = str.substr(found);
         str = ltrim(str);
     }
-    divided.push_back(str.substr(1));
+    divided.push_back(str);
     return divided;
 }
-
-// WORK IN PROGRESS
-// std::vector<std::string>    split(std::string str, std::string del = " \t")
-// {
-//     std::vector<std::string>    divided;
-
-//         str = trim(str, del);
-
-//     while (std::size_t found = str.find(del))
-//     {
-//         divided.push_back(str.substr(0, found));
-
-//         str = str.substr(found);
-//     }
-//     divided.push_back(str.substr(1));
-//     return divided;
-// }
---
+++ srcs/stringFuncs.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+
+std::string ltrim(std::string str);
+std::string trim(std::string str);
+std::string trim(std::string str, std::string undesired);
+std::vector<std::string>    split(std::string str);
